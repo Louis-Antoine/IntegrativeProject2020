c25b2d76baca6bbf40ccb13ea9fd3e80dda16c7e
Louis-Antoine <60940273+Louis-Antoine@users.noreply.github.com>
2021-01-16 14:56:02 -0500

    Add files via upload

---
+++ Project files/core/assets/map/TileTest.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="TileTest" tilewidth="16" tileheight="16" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="14" source="plat_supports.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="14" source="plat_supports_icy.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="Background.png"/>
+ </tile>
+ <tile id="3">
+  <image width="14" height="14" source="../objects/box.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../entity/john_1frame.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../objects/ice_gun.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="../objects/door_closed.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="../objects/pressure_plate_off.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="../objects/switch_off.png"/>
+ </tile>
+</tileset>
